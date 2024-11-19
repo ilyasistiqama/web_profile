6d78fda1f5bac912f5219d7a4f2a6e248029fc79
ilyas <ilyasistiqama@gmail.com>
2024-11-19 14:49:15 +0700

    [add] preloader and favicon

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let embedIsLoad = false;
+
+$(document).ready(() => {
+    let year = new Date();
+
+    $('#year').html("© " + year.getFullYear() + " Copyright: All rights reserved.");
+
+    setTimeout(function () {
+        $('body').addClass('loaded');
+    }, 2000);
+});
+
+
+$('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+
+$(document).on('click', '#cv-btn', function (e) {
+    e.preventDefault();
+
+    let path = 'https://drive.google.com/drive/folders/1ToUoWToqsUVoGj7CRcbUsMxIT5RhuYUC?usp=sharing';
+
+    window.open(path, '_blank');
+});
